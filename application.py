84fc5293a5f8f33474958df424b1e85899a4fa0d
muhammad2k18 <m.ahmad.r.churahi@gmail.com>
2020-05-05 21:47:54 +0500

    fixed path and created new folders

--- application.py
+++ application.py
@@ -1,12 +1,28 @@
-from flask import Flask
+from flask import Flask,render_template,url_for,request,redirect
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return 'Hello world'
+    return render_template('index.html')
 
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+
+@app.route("/patient",methods=['POST'])
+def patientProfile():
+    if request.method == 'POST':
+        return render_template('patient.html')
+
+
+@app.route("/admin",methods=['POST'])
+def dataEntry():
+    if request.method == 'POST':
+        return render_template('Patient_Data_Entry.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
