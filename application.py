f1a99628117fadf1675f79b9c0887c1aa6224da1
Haleema Mustafa <halimamustafa03@gmail.com>
2020-05-06 01:20:58 +0500

    All modified files added@

--- application.py
+++ application.py
@@ -1,5 +1,4 @@
 from flask import Flask,render_template,url_for,request,redirect
-
 app = Flask(__name__)
 
 
@@ -23,6 +22,11 @@ def patientProfile():
 def dataEntry():
     if request.method == 'POST':
         return render_template('Patient_Data_Entry.html')
+
+
+@app.route("/worker")
+def workerProfile():
+    return render_template('Worker_Profile.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
